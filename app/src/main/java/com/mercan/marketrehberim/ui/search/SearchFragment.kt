6771c709b680d3fa2567f30047351581d37a5a58
cusufcan <cusufcan@gmail.com>
2025-01-07 15:22:58 +0300

    add: search bar in SearchFragment

--- app/src/main/java/com/mercan/marketrehberim/ui/search/SearchFragment.kt
+++ app/src/main/java/com/mercan/marketrehberim/ui/search/SearchFragment.kt
@@ -5,16 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
 import com.mercan.marketrehberim.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var etSearch: TextInputEditText
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bindViews()
+    }
+
+    private fun bindViews() {
+        etSearch = binding.etSearch
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
