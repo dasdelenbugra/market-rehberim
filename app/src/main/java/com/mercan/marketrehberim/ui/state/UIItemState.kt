d488ad1480610b985925a2d706f2efe51dcad102
cusufcan <cusufcan@gmail.com>
2025-01-07 16:04:49 +0300

    add: UIItemState

---
+++ app/src/main/java/com/mercan/marketrehberim/ui/state/UIItemState.kt
@@ -0,0 +1,9 @@
+package com.mercan.marketrehberim.ui.state
+
+import com.mercan.marketrehberim.data.model.Item
+
+sealed class UIItemState {
+    data object Loading : UIItemState()
+    data class Success(val items: List<Item>) : UIItemState()
+    data class Error(val message: String) : UIItemState()
+}
