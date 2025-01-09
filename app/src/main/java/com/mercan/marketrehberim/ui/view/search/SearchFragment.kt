8a47bb104b5e25a9742dbcb2cf02f56f33f72fd1
cusufcan <cusufcan@gmail.com>
2025-01-09 12:40:19 +0300

    add: data observer in SearchFragment

--- app/src/main/java/com/mercan/marketrehberim/ui/view/search/SearchFragment.kt
+++ app/src/main/java/com/mercan/marketrehberim/ui/view/search/SearchFragment.kt
@@ -5,16 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import com.mercan.marketrehberim.data.model.Item
 import com.mercan.marketrehberim.databinding.FragmentSearchBinding
+import com.mercan.marketrehberim.ui.adapter.ItemAdapter
+import com.mercan.marketrehberim.ui.state.UIItemState
+import com.mercan.marketrehberim.ui.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(), View.OnFocusChangeListener {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var etSearch: TextInputEditText
+
+    private val searchViewModel: SearchViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -27,14 +37,50 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         bindViews()
+        observeData()
+        fetchData()
     }
 
     private fun bindViews() {
         etSearch = binding.etSearch
     }
 
+    private fun observeData() {
+        lifecycleScope.launch {
+            searchViewModel.searchResults.collect { state ->
+                when (state) {
+                    is UIItemState.Loading -> loadingLogic()
+                    is UIItemState.Success -> successLogic(state.items)
+                    is UIItemState.Error -> errorLogic(state.message)
+                }
+            }
+        }
+    }
+
+    private fun fetchData() {
+        etSearch.onFocusChangeListener = this
+    }
+
+    private fun loadingLogic() {
+
+    }
+
+    private fun successLogic(items: List<Item>) {
+        val adapter = ItemAdapter(items)
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun errorLogic(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onFocusChange(v: View?, hasFocus: Boolean) {
+        val itemName = etSearch.text.toString()
+        searchViewModel.fetchItems(itemName)
     }
 }
