04d28d73db9a9f76c728eb4c6d303f858622e613
cusufcan <cusufcan@gmail.com>
2025-01-09 16:07:56 +0300

    change: item model

--- app/src/main/java/com/mercan/marketrehberim/data/model/Item.kt
+++ app/src/main/java/com/mercan/marketrehberim/data/model/Item.kt
@@ -2,7 +2,7 @@ package com.mercan.marketrehberim.data.model
 
 data class Item(
     val name: String,
-    val description: String,
     val price: String,
-    val imageUrl: String,
+    val image: String,
+    val from: String,
 )
