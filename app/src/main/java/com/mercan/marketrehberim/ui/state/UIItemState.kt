92ee4911ce40a8b9ae13fccd31195d6f1905efdb
cusufcan <cusufcan@gmail.com>
2025-01-09 17:36:00 +0300

    add: Idle in UIItemState

--- app/src/main/java/com/mercan/marketrehberim/ui/state/UIItemState.kt
+++ app/src/main/java/com/mercan/marketrehberim/ui/state/UIItemState.kt
@@ -3,6 +3,7 @@ package com.mercan.marketrehberim.ui.state
 import com.mercan.marketrehberim.data.model.Item
 
 sealed class UIItemState {
+    data object Idle : UIItemState()
     data object Loading : UIItemState()
     data class Success(val items: List<Item>) : UIItemState()
     data class Error(val message: String) : UIItemState()
