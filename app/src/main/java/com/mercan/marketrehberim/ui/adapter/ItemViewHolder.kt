664fd4b39d16f7be0003cb5e9b1de39478add89a
cusufcan <cusufcan@gmail.com>
2025-01-09 17:36:58 +0300

    change: currency symbol in ItemViewHolder

--- app/src/main/java/com/mercan/marketrehberim/ui/adapter/ItemViewHolder.kt
+++ app/src/main/java/com/mercan/marketrehberim/ui/adapter/ItemViewHolder.kt
@@ -9,7 +9,7 @@ class ItemViewHolder(private val binding: ItemLayoutBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(item: Item) {
         binding.name.text = item.name
-        binding.price.text = item.price
+        binding.price.text = "${item.price} ₺"
         binding.from.text = item.from
         Glide.with(binding.root).load(item.image).into(binding.image)
     }
